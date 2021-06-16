b4fd3639747a45f6eb851c8ac60fcc9ae095d8c2
kevin <kevin@app.eng>
2021-06-16 09:27:09 +0900

    Installation shell

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
-cp .vimrc $HOME/.
+cp .bashrc .tmux.conf .vimrc $HOME/.
 
-wget http://cscope.sourceforge.net/cscope_maps.vim -P $HOME/.vim/plugin
+#wget http://cscope.sourceforge.net/cscope_maps.vim -P $HOME/.vim/plugin
 
 mkdir -p $HOME/.vim/colors
-cp evolution.vim $HOME/.vim/colors/
+cp jellybeans.vim evolution.vim $HOME/.vim/colors/
+cp -rf bundle $HOME/.vim/
+
+vim +PluginInstall +qall
 
 echo "You need to restart the shell to apply changes."
